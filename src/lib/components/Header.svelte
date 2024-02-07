e0687a0e6a9c056051d8de0a11a931d62f392263
Jeremy <the.jeremy.cobb@gmail.com>
2024-02-06 21:52:28 -0600

    wrapped code in header tags

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,10 +1,12 @@
 <!-- Header.svelte -->
-<div class="navbar bg-base-100 justify-center">
-    <a href="/" class="btn btn-ghost text-xl">Home</a>
-    <a href="/Fundamentals" class="btn btn-ghost text-xl">Fundamentals</a>
-    <a href="/Tools" class="btn btn-ghost text-xl">Tools</a>
-    <a href="/Methodologies-1" class="btn btn-ghost text-xl">Methodologies I</a>
-    <a href="/Methodologies-2" class="btn btn-ghost text-xl">Methodologies II</a>
-    <a href="/Appendeix" class="btn btn-ghost text-xl">Appendix</a>
-    <a href="/Resources" class="btn btn-ghost text-xl">Resources</a>
-</div>
+<header>
+    <div class="navbar bg-base-100 justify-center">
+        <a href="/" class="btn btn-ghost text-xl">Home</a>
+        <a href="/Fundamentals" class="btn btn-ghost text-xl">Fundamentals</a>
+        <a href="/Tools" class="btn btn-ghost text-xl">Tools</a>
+        <a href="/Methodologies-1" class="btn btn-ghost text-xl">Methodologies I</a>
+        <a href="/Methodologies-2" class="btn btn-ghost text-xl">Methodologies II</a>
+        <a href="/Appendeix" class="btn btn-ghost text-xl">Appendix</a>
+        <a href="/Resources" class="btn btn-ghost text-xl">Resources</a>
+    </div>
+</header>
