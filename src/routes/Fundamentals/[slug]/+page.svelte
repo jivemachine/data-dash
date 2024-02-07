576c8c4d8ed8c8c956a8cbd394af6089cc0b39a9
Jeremy <the.jeremy.cobb@gmail.com>
2024-02-06 23:27:29 -0600

    prose for typography/markdown

---
+++ src/routes/Fundamentals/[slug]/+page.svelte
@@ -0,0 +1,11 @@
+<!-- src/routes/[slug]/+page.svelte -->
+<script>
+	export let data;
+</script>
+<!-- <article>
+	<h1>{data.title}</h1>
+	<svelte:component this={data.content} />
+</article> -->
+<div class="prose prose-lg">
+    <svelte:component this={data.content} />
+</div>
