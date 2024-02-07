84b357dae2c33309c6c5a3d6b1b3de713e542ab0
Jeremy <the.jeremy.cobb@gmail.com>
2024-02-07 10:04:40 -0600

    made a decision on how I'm going to link to different categories and subcategories

---
+++ src/routes/Fundamentals/Data-Science/[slug]/+page.js
@@ -0,0 +1,12 @@
+// src/routes/Fundamentals/Data-Science/[slug]/+page.js
+export async function load({ params }) {
+	const post = await import(`../../data/${params.slug}.md`);
+	const { title, slug } = post.metadata;
+	const Content = post.default;
+
+	return {
+		Content,
+		title,
+        slug
+	};
+}
