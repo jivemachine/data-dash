1d49c15ef0c2708a68bde19e58d5be538152863e
Jeremy <the.jeremy.cobb@gmail.com>
2024-02-07 17:41:45 -0600

    fixed route

--- src/lib/utils/index.js
+++ src/lib/utils/index.js
@@ -1,18 +1,18 @@
 export const fetchMarkdownPosts = async () => {
-    const allPostFiles = import.meta.glob('src/routes/data/*.md');
-    const iterablePostFiles = Object.entries(allPostFiles);
+	const allPostFiles = import.meta.glob('/src/routes/Fundamentals/data/*.md');
+	const iterablePostFiles = Object.entries(allPostFiles);
 
-    const allPosts = await Promise.all(
-        iterablePostFiles.map(async ([path, resolver]) => {
-            const { metadata } = await resolver();
-            const postPath = path.slice(11, -3);
+	const allPosts = await Promise.all(
+		iterablePostFiles.map(async ([path, resolver]) => {
+			const { metadata } = await resolver();
+			const postPath = path.slice(11, -3);
 
-            return {
-                meta: metadata,
-                path: postPath
-            };
-        })
-    );
+			return {
+				meta: metadata,
+				path: postPath
+			};
+		})
+	);
 
-    return allPosts;
+	return allPosts;
 };
