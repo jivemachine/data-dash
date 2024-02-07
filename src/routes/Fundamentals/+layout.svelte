3d9b7d8b9460c08efa7b9670796cfb73fe0e598a
Jeremy <the.jeremy.cobb@gmail.com>
2024-02-07 10:55:16 -0600

    layout for blog style posts and sidebar

--- src/routes/Fundamentals/+layout.svelte
+++ src/routes/Fundamentals/+layout.svelte
@@ -1,4 +1,29 @@
 <!-- Fundamentals/+layout.svelte -->
-<div class="relative mx-auto max-w-prose overflow-hidden bg-white py-16 px-4 sm:px-6 lg:px-8">
-    <slot />
+<!-- <div class="relative mx-auto max-w-prose overflow-hidden bg-white py-16 px-4 sm:px-6 lg:px-8">
+	<slot />
+</div> -->
+<div class="post" data-sveltekit-reload>
+    <ul class="menu bg-base-200 w-56 rounded-box ml-4">
+        <li>
+            <h2 class="menu-title">Fundamentals</h2>
+            <ul>
+                <li><a href="/Fundamentals/Enviroment">Enviroment Setup</a></li>
+                <li>
+                    <details open>
+                        <summary>Data Science</summary>
+                        <ul>
+                            <li><a href="/Fundamentals/Data-Science/Intro">Intro to Data Science</a></li>
+                            <li><a href="/Fundamentals/Data-Science/DS-Vocabulary">Data Science Vocabulary</a></li>
+                            <li><a href="/Fundamentals/Data-Science/Pipeline">The Data Science Pipeline</a></li>
+                            <li><a href="/Fundamentals/Data-Science/Pipeline-Demo">Pipeline Demo</a></li>
+                            <li><a href="/Fundamentals/Data-Science/Project">Project</a></li>
+                        </ul>
+                    </details>
+                </li>
+            </ul>
+        </li>
+    </ul>
+    <main class="relative mx-5 bg-white py-16 px-4 sm:px-6 lg:px-8">
+        <slot />
+    </main>
 </div>
