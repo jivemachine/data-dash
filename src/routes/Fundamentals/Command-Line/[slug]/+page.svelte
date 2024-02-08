956d77093279a8b8d48dd9348169233bf61279e0
Jeremy <the.jeremy.cobb@gmail.com>
2024-02-07 18:21:59 -0600

    added command line slug and fetching script

---
+++ src/routes/Fundamentals/Command-Line/[slug]/+page.svelte
@@ -0,0 +1,14 @@
+<!-- src/routes/Fundamentals/Command-Line/[slug]/+page.svelte -->
+<script>
+	export let data;
+	const { title, slug, Content } = data;
+</script>
+
+<h1 class="text-3xl bold mb-5">
+	<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-8 h-8 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 10V3L4 14h7v7l9-11h-7z"></path></svg>
+	{title}
+</h1>
+
+<article class="prose prose-lg prose-headings:underline prose-a:no-underline prose-a:link-primary hover:prose-a:underline prose-img:rounded-xl">
+	<Content />
+</article>
