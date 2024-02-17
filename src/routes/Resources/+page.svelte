c31cd62824d6fc4475ca62281febf3f121fe8e29
Jeremy <the.jeremy.cobb@gmail.com>
2024-02-17 17:45:18 -0600

    added pdf and pdf component

--- src/routes/Resources/+page.svelte
+++ src/routes/Resources/+page.svelte
@@ -1,1 +1,10 @@
-<!-- Resources/+pages.svelte -->
+<script>
+	import PdfViewer from 'svelte-pdf';
+</script>
+
+<PdfViewer 
+    url="src/routes/Resources/Codeup_Syllabus_-_ursula.pdf"
+    scale={1.5}
+    showButtons={["navigation", "zoom", "print", "autoflip", "timeInfo", "pageInfo"]}
+    showBorder={true}
+/>
